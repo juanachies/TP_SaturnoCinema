94280d15d586316a07002cc591ee73cc538953e0
juana <jchiesdoumecq@gmail.com>
2025-09-11 09:24:46 -0300

    arreglando

--- SaturnoCinema/src/App.jsx
+++ SaturnoCinema/src/App.jsx
@@ -1,14 +1,14 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { useState } from 'react'
 import './App.css'
 import Dashboard from './components/dashboard/Dashboard'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Dashboard/>} /> 
-      </Routes>    
-    </BrowserRouter>
+    <>
+      <Dashboard/>
+    </>
   )
 }
 
