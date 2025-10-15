c759bdcee7af6a035cf0551553dcc622f90aa89b
juana <jchiesdoumecq@gmail.com>
2025-10-14 23:47:14 -0300

    Get y post de reservas hecho

--- SaturnoCinemaAPI/src/index.js
+++ SaturnoCinemaAPI/src/index.js
@@ -3,6 +3,7 @@ import { PORT } from "./config.js";
 import express from 'express';
 import movieRoutes from './routes/movie.routes.js'
 import authRoutes from './routes/auth.routes.js'
+import reservationRoutes from './routes/reservation.routes.js'
 import { sequelize } from "./db.js";
 
 const app = express()
@@ -19,6 +20,7 @@ try{
 
     app.use(movieRoutes)
     app.use(authRoutes)
+    app.use(reservationRoutes)
     app.listen(PORT)
     
     await sequelize.sync();
