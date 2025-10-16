0668abfcbba368863cb9b98b10c6605da447113c
juana <jchiesdoumecq@gmail.com>
2025-10-16 09:27:06 -0300

    bdd

--- SaturnoCinema/src/routing/Protected.jsx
+++ SaturnoCinema/src/routing/Protected.jsx
@@ -1,9 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router";
 
 const Protected = ({children, allowedTypes}) => {
     const token = localStorage.getItem("token");
     const user = JSON.parse(localStorage.getItem("user"));
     const userType = user?.type
+
+    const navigate = useNavigate()
+
+    if (!token){
+        return <Navigate to='/login' replace />
+    }
+
+
 }
 
 export default Protected
