2ed078860585f69d109861c6af8f9a003186e1c9
Ernesto Londero <ernestolondero@gmail.com>
2025-10-23 10:58:17 -0300

    Incorporando page notFound

---
+++ SaturnoCinema/src/components/notFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import './notFound.css';
+
+export const NotFound = () => {
+
+    return (
+        <>
+            <i class="bi bi-ban"></i>
+            <h2>
+                Ups! Usted no tiene autorizacion para esto. Lo redireccionaremos a la pagina principa.
+            </h2>
+            <button>
+
+            </button>
+        </>
+    )
+}
